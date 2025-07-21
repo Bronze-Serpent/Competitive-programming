0de61ca8cd986eab8d46763b435003269917cb64
Sequ3nce <PRBar002@gmail.com>
2025-07-21 10:10:25 +0300

    Добавляет альтернативное решение для задачи с leetcode

--- leetcode/src/main/java/com/barabanov/leetcode/top/sql50/GamePlayAnalysisIV.sql
+++ leetcode/src/main/java/com/barabanov/leetcode/top/sql50/GamePlayAnalysisIV.sql
@@ -13,3 +13,16 @@ SELECT ROUND(COUNT(*) / (SELECT * FROM player_count), 2) fraction
 FROM Activity
 WHERE (player_id, DATE_SUB(event_date,  INTERVAL 1 DAY))
     IN (SELECT * FROM first_login)
+
+
+-- решение для postgres
+-- Write your PostgreSQL query statement below
+WITH players_first_login AS (
+    SELECT a.player_id player_id, MIN(event_date) player_first_event
+    FROM activity a
+    GROUP BY a.player_id)
+SELECT ROUND(COUNT(*)::numeric / (SELECT COUNT(*)
+                                  FROM players_first_login), 2) fraction
+FROM activity a
+WHERE (a.player_id, a.event_date - 1) IN (SELECT *
+                                          FROM players_first_login)
